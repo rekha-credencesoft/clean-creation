6b07d8739d5230e482d09f3b950c3b01fa2302a2
zuhair-credencesoft <103411878+zuhair-credencesoft@users.noreply.github.com>
2022-10-10 16:02:14 +0530

    FAQ Page Responsive Links Working Style Added and Completed.

--- components/Faq/Faq.jsx
+++ components/Faq/Faq.jsx
@@ -16,40 +16,44 @@ const Faq = () => {
         <h3>Frequently Asked Questions</h3>
         <div className={styles.collapseContent}>
           <h4>How do I get started?</h4>
-          <BsPlusCircle
-            className={styles.plusIcon}
-            onClick={() => setOpen({ ...open, button1: !open.button1 })}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-          />
-          <div className={styles.borderLine}></div>
-          <Collapse Collapse in={open.button1}>
-            <div id="example-collapse-text" className={styles.info}>
-              Please visit the “Get Started” link on our website to set up an
-              account. Once a profile is created, you can then visit the “Order”
-              tab on our homepage to begin placing your order.
-            </div>
-          </Collapse>
+          <div>
+            <BsPlusCircle
+              className={styles.plusIcon}
+              onClick={() => setOpen({ ...open, button1: !open.button1 })}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            />
+          </div>
         </div>
+        <hr />
+        <Collapse Collapse in={open.button1}>
+          <div id="example-collapse-text" className={styles.info}>
+            Please visit the “Get Started” link on our website to set up an
+            account. Once a profile is created, you can then visit the “Order”
+            tab on our homepage to begin placing your order.
+          </div>
+        </Collapse>
         {/* /////// */}
         <div className={styles.collapseContent}>
           <h4>How do I get started?</h4>
-          <BsPlusCircle
-            className={styles.plusIcon}
-            button
-            onClick={() => setOpen({ ...open, button2: !open.button2 })}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-          />
-          <div className={styles.borderLine}></div>
-          <Collapse Collapse in={open.button2}>
-            <div id="example-collapse-text" className={styles.info}>
-              Please visit the “Get Started” link on our website to set up an
-              account. Once a profile is created, you can then visit the “Order”
-              tab on our homepage to begin placing your order.
-            </div>
-          </Collapse>
+          <div className={styles.addFaqIcon}>
+            <BsPlusCircle
+              className={styles.plusIcon}
+              button
+              onClick={() => setOpen({ ...open, button2: !open.button2 })}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            />
+          </div>
         </div>
+        <hr />
+        <Collapse Collapse in={open.button2}>
+          <div id="example-collapse-text" className={styles.info}>
+            Please visit the “Get Started” link on our website to set up an
+            account. Once a profile is created, you can then visit the “Order”
+            tab on our homepage to begin placing your order.
+          </div>
+        </Collapse>
       </div>
     </>
   );
